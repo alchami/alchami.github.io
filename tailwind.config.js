a4ff83429402a8b782ddd0bdca82989a02cd8fea
alchami <alchami@riseup.net>
2023-01-26 14:55:35 +0100

    Add Tailwind and PostCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        colors: {
+            'aliceblue': f0f8ff,
+        },
+        fontFamily : {},
+        extend : {},
+    },
+    plugins: [],
+}

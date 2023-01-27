e191fe2277ef38111059eb087956e1277ceff097
alchami <alchami@riseup.net>
2023-01-27 11:23:53 +0100

    And I wondered why color classes weren’t working…

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,vue}"],
-    theme: {
-        colors: {
-            'aliceblue': '#f0f8ff',
-        },
-        fontFamily : {},
-        extend : {},
-    },
-    plugins: [],
+  content: ["./src/**/*.{html,js,vue}"],
+  theme: {
+    extend: {colors: {'aliceblue': '#f0f8ff'}},
+  },
+  plugins: [],
 }

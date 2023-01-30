fb8d2500d5b6f4cd2278b144f79367ad8582f3a6
alchami <alchami@riseup.net>
2023-01-30 19:02:53 +0100

    Add custom fonts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,13 +4,16 @@
         TODO: extract the magic numbers, make div mb, h1 pb,
         img w and bottom dependent of each other
     -->
+    <header>
     <div class="relative mb-[20vw] bg-random-shapes bg-red-400">
-        <h1 class="pt-[10vw] pb-[20vw] text-[10vw] text-center text-white">Alice Chaminas ✨</h1>
+        <h1 class="font-mono pt-[10vw] pb-[20vw] text-[10vw] text-center text-white">Alice Chaminas ✨</h1>
         <img class=" 
             aspect-square w-[40vw]
             mx-auto left-0 right-0
             absolute -bottom-[20vw]
-            rounded-full border-8 border-white"
+            rounded-full border-[3vw] border-white"
         src="assets/avatar.jpg" alt="Avatar représentant Alice"/>
+        <h2 class="font-mono text-center">Developpeuse front-end</h2>
     </div>
+    </header>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
+    fontFamily: {
+      'sans': ['Rubik', 'sans-serif'],
+      'mono': ['"IBM Plex Mono Bold"'],
+    },
     extend: {
       colors: {'aliceblue': '#f0f8ff'},
       backgroundImage: {'random-shapes': "url('assets/random-shapes.svg')"}

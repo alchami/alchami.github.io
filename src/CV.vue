7a34c08edd2d1f2516c68f7a03c786e424b067fc
alchami <alchami@riseup.net>
2023-01-30 19:03:11 +0100

    Better structure

--- src/CV.vue
+++ src/CV.vue
@@ -4,27 +4,33 @@ import Header from "./components/Header.vue";
 import CardList from "./components/CardList.vue";
 import cv from "./cv.json";
 
-console.log(cv.techs);
 </script>
 <template>
   <Header/>
-  <div>
-  <!-- h1 and h2 are used in Header, so h3 is it -->
-    <h3>Mes compétences :</h3>
-    <CardList :data="cv.techs"/>
-  </div>
-  <div>
-    <h3>Ce que j’apprend :</h3>
-    <CardList :data="cv.learning"/>
-  </div>
+  <main>
+    <section>
+    <!-- h1 and h2 are used in Header, so h3 is it -->
+      <h3>Mes technologies :</h3>
+      <CardList :data="cv.techs"/>
+    </section>
 
-  <div>
-    <h3>Mes expériences professionelles :</h3>
-    <CardList :data="cv.jobs"/>
-  </div>
+    <section>
+      <h3>Ce que j’apprend :</h3>
+      <CardList :data="cv.learning"/>
+    </section>
 
-  <div>
-    <h3>Contact : </h3>
-    <CardList :data="cv.socials"/>
-  </div>
+    <section>
+      <h3>Formations et expériences professionelles :</h3>
+      <CardList :data="cv.jobs"/>
+    </section>
+
+    <section>
+      <h3>Langues :</h3>
+      <CardList :data="cv.langs"/>
+    </section>
+    <section>
+      <h3>Contact : </h3>
+      <CardList :data="cv.socials"/>
+    </section>
+  </main>
 </template>
